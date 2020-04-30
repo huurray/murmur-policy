441dd7a468d9d3edb50a2732d0c2d370c3497cf2
huurray <huurray@gmail.com>
2020-04-30 16:39:29 +0900

    Updates

---
+++ precache-manifest.274d489ff9239a4a9cf971a66f268757.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aa8b20dd46a4546b8b5d5e833fce376",
+    "url": "/murmur-policy/index.html"
+  },
+  {
+    "revision": "9c9ee1661a99b9ac15ae",
+    "url": "/murmur-policy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1b9fe5541deac35a6286",
+    "url": "/murmur-policy/static/js/2.c029618a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/murmur-policy/static/js/2.c029618a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c9ee1661a99b9ac15ae",
+    "url": "/murmur-policy/static/js/main.702d9d3b.chunk.js"
+  },
+  {
+    "revision": "8e89b6ab2a3486d7849f",
+    "url": "/murmur-policy/static/js/runtime-main.1230dd35.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/murmur-policy/static/media/logo.5d5d9eef.svg"
+  }
+]);
